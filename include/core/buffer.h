9a865e6d24dd0de989c4ab90fcf78447121ec6dd
nvllmnd <nvllmnd@pm.me>
2026-05-15 16:16:07 -0700

    add string to buffer.h

--- include/core/buffer.h
+++ include/core/buffer.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "attributes.h"
 #include "intdefs.h"
 #include "memory/alloc.h"
 
@@ -108,24 +109,21 @@ METHOD
 PURE_FUNC
 const u8* buff_cend(const Buff* self);
 
-
 #define buff_for_i(_self_, _index_name_) /* convienence macro for iterating over a vec. second parameter is just \
-the loop index variable name. [vec_foreach] uses i' by default */                                                                                                   \
+the loop index variable name. [vec_foreach] uses i' by default */                                                \
   for (int _index_name_ = 0; _index_name_ < buff_len((_self_)); _index_name_++)
 
 #define buff_for(_self_) /* same as [vec_for_i] macro, but sets _index_name_ = i*/ buff_for_i(_self_, i)
 
-#define buff_foreach_iter(_self_, _iter_name_) for (__typeof(*(_self_))* _iter_name_ = _self_; _iter_name_ < buff_end((_self_)); _iter_name_++)
+#define buff_foreach_iter(_self_, _iter_name_) \
+  for (__typeof(*(_self_))* _iter_name_ = _self_; _iter_name_ < buff_end((_self_)); _iter_name_++)
 
 #define buff_foreach(_self_) buff_foreach_iter(_self_, iter)
 
-
-#define buff_foreach_iter_const(_self_, _iter_name_) for (const __typeof(*(_self_))* _iter_name_ = _self_; _iter_name_ < buff_end((_self_)); _iter_name_++)
-
+#define buff_foreach_iter_const(_self_, _iter_name_) \
+  for (const __typeof(*(_self_))* _iter_name_ = _self_; _iter_name_ < buff_end((_self_)); _iter_name_++)
 
 #define buff_foreach_const(_self_) buff_foreach_iter_const(_self_, iter)
-
-
 
 #define Vec(T) ptr(T)
 
@@ -163,7 +161,8 @@ the loop index variable name. [vec_foreach] uses i' by default */               
 
 #define vec_clear(self) (buff_clear(pcast(Buff, (self))))
 
-#define vec_write(self, out, out_len) (buff_write(pcast(Buff, (self)), (out), (out_len) * (i32)sizeof(__typeof(*(self)))))
+#define vec_write(self, out, out_len) \
+  (buff_write(pcast(Buff, (self)), (out), (out_len) * (i32)sizeof(__typeof(*(self)))))
 #define vec_resize(self, new_capacity, alloc) \
   (buff_resize(pcast(Buff, (self)), (new_capacity) * (i32)sizeof(__typeof(*(self))), (alloc)))
 
@@ -174,17 +173,97 @@ the loop index variable name. [vec_foreach] uses i' by default */               
 #define vec_min_size buff_min_size
 
 #define vec_for_i(_self_, _index_name_) /* convienence macro for iterating over a vec. second parameter is just \
-the loop index variable name. [vec_foreach] uses i' by default */                                                                                                   \
+the loop index variable name. [vec_foreach] uses i' by default */                                               \
   for (int _index_name_ = 0; _index_name_ < vec_len((_self_)); _index_name_++)
 
 #define vec_for(_self_) /* same as [vec_for_i] macro, but sets _index_name_ = i*/ vec_for_i(_self_, i)
 
-#define vec_foreach_iter(_self_, _iter_name_) for (__typeof(*(_self_))* _iter_name_ = _self_; _iter_name_ < vec_end((_self_)); _iter_name_++)
+#define vec_foreach_iter(_self_, _iter_name_) \
+  for (__typeof(*(_self_))* _iter_name_ = _self_; _iter_name_ < vec_end((_self_)); _iter_name_++)
 
 #define vec_foreach(_self_) vec_foreach_iter(_self_, iter)
 
-
-#define vec_foreach_iter_const(_self_, _iter_name_) for (const __typeof(*(_self_))* _iter_name_ = _self_; _iter_name_ < vec_end((_self_)); _iter_name_++)
+#define vec_foreach_iter_const(_self_, _iter_name_) \
+  for (const __typeof(*(_self_))* _iter_name_ = _self_; _iter_name_ < vec_end((_self_)); _iter_name_++)
 
 #define vec_foreach_const(_self_) vec_foreach_iter_const(_self_, iter)
 
+/// A manually resizable String that may or may not be null terminated
+typedef Vec(char) String;
+
+static HEDLEY_ALWAYS_INLINE String string_new(i32 capacity, Allocator alloc) { return vec_new(char, capacity, alloc); }
+
+METHOD
+static HEDLEY_ALWAYS_INLINE sslice string_npush(String self, const char* string, i32 n) {
+  return buff_append_nstr((Buff*)self, string, n);
+}
+
+METHOD
+static HEDLEY_ALWAYS_INLINE sslice string_push(String self, const char* string) { return buff_append_str((Buff*)self, string); }
+
+METHOD
+PURE_FUNC
+static HEDLEY_ALWAYS_INLINE i32 string_len(const String self) { return vec_len(self); }
+
+METHOD
+PURE_FUNC
+static HEDLEY_ALWAYS_INLINE  i32 string_capacity(const String self) { return vec_capacity(self); }
+
+METHOD
+static HEDLEY_ALWAYS_INLINE  char* string_end(String self) { return vec_end(self); }
+
+METHOD
+PURE_FUNC
+static HEDLEY_ALWAYS_INLINE const char* string_cend(const String self) { return vec_cend(self); }
+
+METHOD
+PURE_FUNC
+static HEDLEY_ALWAYS_INLINE i32 string_available(const String self) { return buff_available((const Buff*)self); }
+
+METHOD
+static HEDLEY_ALWAYS_INLINE void string_destroy(String self, Allocator alloc) { buff_destroy((Buff*)self, alloc); }
+
+METHOD
+static HEDLEY_ALWAYS_INLINE void string_clear_zeroed(String self) { buff_clear_zeroed((Buff*)self); }
+
+METHOD
+static HEDLEY_ALWAYS_INLINE void string_clear(String self) { (buff_clear(pcast(Buff, (self)))); }
+
+METHOD
+static HEDLEY_ALWAYS_INLINE String string_resize(String self, i32 new_capacity, Allocator alloc) {
+  return (String)(buff_resize(pcast(Buff, (self)), (new_capacity), (alloc)));
+}
+
+static HEDLEY_ALWAYS_INLINE String string_from_mem(char* start, char* end) {
+  return (String)buff_from_mem(pcast(u8, start), pcast(u8, end));
+}
+
+PURE_FUNC
+METHOD
+static HEDLEY_ALWAYS_INLINE bool string_is_full(const String self) {
+  return vec_is_full(self);
+}
+
+PURE_FUNC
+METHOD
+static HEDLEY_ALWAYS_INLINE bool string_is_empty(const String self) {
+  return vec_is_empty(self);
+}
+
+#define string_min_size buff_min_size
+
+#define string_for_i(_self_, _index_name_) /* convienence macro for iterating over a vec. second parameter is just \
+the loop index variable name. [vec_foreach] uses i' by default */                                               \
+  for (int _index_name_ = 0; _index_name_ < vec_len((_self_)); _index_name_++)
+
+#define string_for(_self_) /* same as [vec_for_i] macro, but sets _index_name_ = i*/ vec_for_i(_self_, i)
+
+#define string_foreach_iter(_self_, _iter_name_) \
+  for (__typeof(*(_self_))* _iter_name_ = _self_; _iter_name_ < vec_end((_self_)); _iter_name_++)
+
+#define string_foreach(_self_) vec_foreach_iter(_self_, iter)
+
+#define string_foreach_iter_const(_self_, _iter_name_) \
+  for (const __typeof(*(_self_))* _iter_name_ = _self_; _iter_name_ < vec_end((_self_)); _iter_name_++)
+
+#define string_foreach_const(_self_) vec_foreach_iter_const(_self_, iter)
