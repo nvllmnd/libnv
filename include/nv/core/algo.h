8f097c549722704277e4b8ecfbfe62d38821afeb
nvllmnd <nvllmnd@pm.me>
2026-07-14 11:27:09 -0700

    fix algo.h for C++

--- include/nv/core/algo.h
+++ include/nv/core/algo.h
@@ -8,6 +8,17 @@
 #include "nv/core/intdefs.h"
 #include "nv/core/sslice.h"
 
+#ifndef STRLEN_UPPER_BOUND
+
+#define STRLEN_UPPER_BOUND                                                    \
+  /* Upper bound used by [stringlen] as the max_len parameter to [str_len] */ \
+  /* NOTE: I decided to make this a macro so that it can be configurable to   \
+   * each build (-D compiler flag)*/                                          \
+  (INT64_MAX - 1)
+
+#endif  // STRLEN_UPPER_BOUND
+
+
 #ifndef __cplusplus
 
 
@@ -335,28 +346,6 @@ typedef BytesOf(isize) IsizeBytes;
 typedef BytesOf(f32) FloatBytes;
 typedef BytesOf(f64) Float64Bytes;
 
-HEDLEY_PRINTF_FORMAT(1, 2)
-PURE_FUNC
-i32 fstring_length(const char* fmt, ...);
-
-/// @brief Determines printf-style format string resulting length excluding, null-terminator
-/// @details does not modify va_list args
-PURE_FUNC
-i32 vfstring_length(const char* fmt, va_list args);
-
-BEGIN_C_DECLS
-
-PURE_FUNC
-PARAMS_NONNULL(1)
-u32 fnv_hash32(const char* string, isize len) WHERE(len > 0);
-
-PURE_FUNC
-PARAMS_NONNULL(1)
-u64 fnv_hash64(const char* string, isize len) WHERE(len > 0);
-
-CONST_FUNC
-static inline bool is_power_of_2(isize n) { return IS_POWER_OF_2(n); }
-
 /// @brief a safer, more efficient [strncat]
 /// @details Its not required that dest string ends with a null terminal, however after this function returns
 /// there is gauranteed to be a null terminal, which will get overwritten on the next invokation of this function using
@@ -375,46 +364,6 @@ NvError try_stringcat(char* dest, i64 dest_count, i64 dest_size, const char* src
 /// @returns  new character count of destination string, not including null terminal. You can use this value to
 /// pass to the next invokation of stringcat
 i64 stringcat(char* dest, i64 dest_count, i64 dest_size, const char* src, i64 srclen) PARAMS_NONNULL(1, 4);
-
-// CLANG_NON_NULL_BEGIN
-
-// struct NonNull {
-//   void* CLANG_NON_NULL ptr;
-// };
-// typedef struct NonNull NonNull;
-
-/// Takes any pointer and if it is null, aborts execution with [log_fatal]. otherwise returns
-/// the same pointer unchanged/un-mutated.
-/// This is to assert to the compiler that a poitner is not null, as
-/// this function is marked with the __returns_nonnull__ compiler attribute
-RETURNS_NON_NULL
-void* ptr_nonnull_(const void* ptr) WHERE(ptr_nonnull_(ptr) == ptr);
-
-// CLANG_NON_NULL_END
-
-#define ptr_nonnull(_ptr) cast(typeof_ptr(_ptr), ptr_nonnull_((const void*)(_ptr)))
-
-/// Same as [ptr_nonnull], but fails with a user provided, message.
-/// NOTE: Because this function is inteded to be used in hot paths, the user
-/// must build error messages themselves. if this function were a printf-style function, it
-/// would at the very worst, pass a bunch of parameters onto the stack that will never be used most the time, callers
-/// are also encouraged to keep messages light (no formatting, just as simple string literal)
-RETURNS_NON_NULL
-void* ptr_expect_(const void* ptr, const char* msg);
-
-#if !defined(pexpect) && (!defined(LIBNV_NO_USE_SHORT_NAMES) || LIBNV_NO_USE_SHORT_NAMES == 0)
-#define pexpect(_p, _msg) cast(typeof_ptr(_p), ptr_expect_((const void*)(_p), (_msg)))
-#endif
-
-#ifndef STRLEN_UPPER_BOUND
-
-#define STRLEN_UPPER_BOUND                                                    \
-  /* Upper bound used by [stringlen] as the max_len parameter to [str_len] */ \
-  /* NOTE: I decided to make this a macro so that it can be configurable to   \
-   * each build (-D compiler flag)*/                                          \
-  (INT64_MAX - 1)
-
-#endif  // STRLEN_UPPER_BOUND
 
 /// A safe version of the standard lib: [strlen], which technically may never
 /// return if the passed in string never contains a null character to signal
@@ -453,6 +402,38 @@ bool stringeq(const char* left, const char* right);
 
 BEGIN_C_DECLS
 
+
+// CLANG_NON_NULL_BEGIN
+
+// struct NonNull {
+//   void* CLANG_NON_NULL ptr;
+// };
+// typedef struct NonNull NonNull;
+
+/// Takes any pointer and if it is null, aborts execution with [log_fatal]. otherwise returns
+/// the same pointer unchanged/un-mutated.
+/// This is to assert to the compiler that a poitner is not null, as
+/// this function is marked with the __returns_nonnull__ compiler attribute
+RETURNS_NON_NULL
+void* ptr_nonnull_(const void* ptr) WHERE(ptr_nonnull_(ptr) == ptr);
+
+// CLANG_NON_NULL_END
+
+#define ptr_nonnull(_ptr) cast(typeof_ptr(_ptr), ptr_nonnull_((const void*)(_ptr)))
+
+/// Same as [ptr_nonnull], but fails with a user provided, message.
+/// NOTE: Because this function is inteded to be used in hot paths, the user
+/// must build error messages themselves. if this function were a printf-style function, it
+/// would at the very worst, pass a bunch of parameters onto the stack that will never be used most the time, callers
+/// are also encouraged to keep messages light (no formatting, just as simple string literal)
+RETURNS_NON_NULL
+void* ptr_expect_(const void* ptr, const char* msg);
+
+#if !defined(pexpect) && (!defined(LIBNV_NO_USE_SHORT_NAMES) || LIBNV_NO_USE_SHORT_NAMES == 0)
+#define pexpect(_p, _msg) cast(typeof_ptr(_p), ptr_expect_((const void*)(_p), (_msg)))
+#endif
+
+
 /// @brief Determines printf-style format string resulting length, excluding null-terminator
 
 /// @brief concat no more than dest_len bytes of expanded printf-style string to dest
@@ -475,6 +456,29 @@ Endianness endianness(void);
 
 #endif // ifndef __cplusplus
 
+
+
+HEDLEY_PRINTF_FORMAT(1, 2)
+PURE_FUNC
+i32 fstring_length(const char* fmt, ...);
+
+/// @brief Determines printf-style format string resulting length excluding, null-terminator
+/// @details does not modify va_list args
+PURE_FUNC
+i32 vfstring_length(const char* fmt, va_list args);
+
+BEGIN_C_DECLS
+
+PURE_FUNC
+PARAMS_NONNULL(1)
+u32 fnv_hash32(const char* string, isize len) WHERE(len > 0);
+
+PURE_FUNC
+PARAMS_NONNULL(1)
+u64 fnv_hash64(const char* string, isize len) WHERE(len > 0);
+
+CONST_FUNC
+static inline bool is_power_of_2(isize n) { return IS_POWER_OF_2(n); }
 
 
 PURE_FUNC
